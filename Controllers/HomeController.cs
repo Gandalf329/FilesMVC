f572f36d3b57ce53cc6b4a8c00a2eae92577fbfa
Gandalf329 <pohlebaev.n@yandex.ru>
2022-05-11 22:57:55 +0300

    Add image addition and view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,27 @@
 ﻿using FilesMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 namespace FilesMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationContext context, IWebHostEnvironment appEnvironment)
         {
-            _logger = logger;
+            _context = context;
+            _appEnvironment = appEnvironment;
         }
-
+        
         public IActionResult Index()
         {
             return View();
@@ -32,6 +36,30 @@ namespace FilesMVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult AllFiles()
+        {
+            return View(_context.Files.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // path to Files folder
+                string path = "/Files/" + uploadedFile.FileName;
+                
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("AllFiles");
         }
     }
 }
